bc717a45065a31be84776a71e1186ee29664bdc3
Mooner510 <lmck510@gmail.com>
2023-02-03 18:03:38 +0900

    Initial commit

---
+++ src/main/java/org/mooner/seungwoomaster/SeungWooMaster.java
@@ -0,0 +1,17 @@
+package org.mooner.seungwoomaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SeungWooMaster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
