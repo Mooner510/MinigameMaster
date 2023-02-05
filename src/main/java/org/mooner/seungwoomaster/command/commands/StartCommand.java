75dcd52feed152717bd39fe23b38af1f0f17525d
Mooner510 <lmck510@gmail.com>
2023-02-05 23:51:08 +0900

    Added args to StartCommand

--- src/main/java/org/mooner/seungwoomaster/command/commands/StartCommand.java
+++ src/main/java/org/mooner/seungwoomaster/command/commands/StartCommand.java
@@ -3,11 +3,21 @@ package org.mooner.seungwoomaster.command.commands;
 import org.bukkit.entity.Player;
 import org.mooner.seungwoomaster.command.ICommand;
 import org.mooner.seungwoomaster.game.GameManager;
+import org.mooner.seungwoomaster.game.PlayMap;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class StartCommand implements ICommand {
     @Override
     public boolean execute(Player sender, String[] args) {
-        GameManager.getInstance().start();
+        if (args.length <= 0) GameManager.getInstance().start();
+        else GameManager.getInstance().start(PlayMap.valueOf(args[0]));
         return true;
+    }
+
+    @Override
+    public List<String> tabComplete(Player sender, String[] args) {
+        return Arrays.stream(PlayMap.values()).map(Enum::toString).filter(it -> it.startsWith(args[0])).toList();
     }
 }
